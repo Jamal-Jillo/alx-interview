92b9b2266ff6d4aa4c253c5c3482a4ac5057efac
Jamal <jayjamal86@gmail.com>
2023-04-05 19:46:44 +0300

    factorization was returning wrong values

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
 """Minimum Operations."""
 
-import math
-
 
 def minOperations(n):
     """Calculate the fewest number of operations needed to result in exactly n H characters in the file."""
-    if n == 1:
+    if n < 1:
         return 0
 
-    # find prime factorization of n
-    factors = {}
     i = 2
-    while i*i <= n:
+    operations = 0
+
+    while n > 1:
         while n % i == 0:
-            if i not in factors:
-                factors[i] = 0
-                print(factors)
-            factors[i] += 1
+            operations += i
             n //= i
         i += 1
-    if n > 1:
-        factors[n] = 1
-        print(factors)
 
-    # calculate minimum number of operations
-    min_ops = 0
-    for exp in factors.values():
-        min_ops += exp
-
-    print(min_ops)
-    return min_ops
+    return operations
