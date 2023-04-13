5659b720034e33bbabdd07268c9e989decc6d79e
Jamal <jayjamal86@gmail.com>
2023-04-13 11:47:21 +0300

    fixed key error and added test file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,21 +2,21 @@
 """Log parsing"""
 
 
-from sys import stdin
+import sys
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
 total_size = 0
-
 try:
     # read line by line
-    for line in stdin:
+    for line in sys.stdin:
         try:
             tokens = line.split()
-            status_code = int(tokens[8])
-            file_size = int(tokens[9])
+            status_code = int(tokens[-2])
+            file_size = int(tokens[-1])
         except (IndexError, ValueError):
             # skip line if it doesn't have the right format
+            print("Invalid line format:")
             continue
 
         # update stats
